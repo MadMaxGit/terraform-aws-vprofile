af8a70379c88030ce8586f4112718c54cace67a1
MadMaxGit <kpmanjunath1024@gmail.com>
2024-03-26 11:35:49 +0100

    resolved the bug

--- bean-app.tf
+++ bean-app.tf
@@ -1,3 +1,3 @@
-resource "aws_elastic_beanstalk_application" "vprfile-prod" {
-  name = "vprfile-prod"
+resource "aws_elastic_beanstalk_application" "vprofile-prod" {
+  name = "vprofile-prod"
 }
--- bean-env.tf
+++ bean-env.tf
@@ -1,6 +1,6 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   name                = "vprofile-bean-prod"
-  application         = aws_elastic_beanstalk_application.vprfile-prod
+  application         = aws_elastic_beanstalk_application.vprofile-prod.name
   solution_stack_name = "64bit Amazon Linux 2023 v5.1.4 running Tomcat 9 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-domain"
 
